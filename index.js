9c40f39d02c1103f96e97cc560bd6684b3582cf3
Gil <gilmedeiros75@hotmail.com>
2023-10-04 21:46:58 -0300

    mongoose config

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const { default: mongoose } = require("mongoose");
 const localStrategy = require("passport-local").Strategy;
 
 //Config
@@ -11,6 +12,7 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-
+//Mongoose Config
+mongoose.connect("mongodb://localhost:27017/aadb",{useNewUrlParser:true, useUnifiedTopology:true})
 
 app.listen(80, ()=>{console.log("Server ON")})
